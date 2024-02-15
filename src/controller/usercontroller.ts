fd09bb397a6973481a6457049fcb40ab75f97b2e
mohammed zayed <mozayed7736@gmail.com>
2024-02-15 10:26:44 +0530

    Some changes

--- src/controller/usercontroller.ts
+++ src/controller/usercontroller.ts
@@ -14,10 +14,18 @@ export const userPost = async(req:Request,res:Response)=>{
     try {
         const {email,pass} = req.body;
         if(email==process.env.gmail && pass == process.env.password){
-            res.send("verifyed")
+            res.redirect('/home')
         }else{
             res.render("index",{message:"password is wrong"})
         }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const home = async(req:Request,res:Response)=>{
+    try {
+        res.render('home')
     } catch (error) {
         console.log(error);
     }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,12 +3,12 @@ import path from 'path';
 
 const userRoute = express();
 
-import {user,userPost} from '../controller/usercontroller'
+import {user,userPost,home} from '../controller/usercontroller'
 userRoute.use(express.json());
 userRoute.use(express.urlencoded({extended:true}));
 userRoute.set('views', path.join(__dirname, '../views'));
 
 userRoute.get('/',user);
 userRoute.post('/',userPost)
-
+userRoute.get('/home',home)
 export default userRoute;
